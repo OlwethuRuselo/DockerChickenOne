a267a7553462f70e8797cf34c8fcba7c3ea9d3e6
OlwethuRuselo <st10365670@vcconnect.edu.za>
2026-05-21 08:56:18 +0200

    Commit

---
+++ ChickenAPI/Program.cs
@@ -0,0 +1,46 @@
+using ChickenAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ChickenAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<FarmDbContext>(options => 
+                options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING")));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (true)
+            {
+                app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                {
+                    //Tell swagger ui to look at the native .NET OpenApi endpoint
+                    options.SwaggerEndpoint("/openapi/v1.json", "Chicken API v1");
+                    options.RoutePrefix = "swagger";
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
